4715e0efe47d6577dafecdd2ea647427714d37db
Yuri <urabuchko10@gmail.com>
2025-06-20 12:52:24 +0300

    test

--- scripts/script.js
+++ scripts/script.js
@@ -19,21 +19,47 @@ const fetchIpData = async () => {
     const { ip, city, country, postal, timezone, org, latitude, longitude } =
       data;
 
-    var map = L.map("map").setView([latitude, longitude], 15);
-    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-      maxZoom: 19,
-      attribution:
-        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    }).addTo(map);
-
-    address.textContent = ip;
-    loc.textContent = `${city}, ${country} ${postal}`;
-    tmz.textContent = timezone;
-    isp.textContent = org;
+    renderMap(latitude, longitude);
+    updateUI(ip, city, country, postal, timezone, org);
   } catch (error) {
     console.log(error);
   }
 };
 
-// fetchIpData();
+let map = null;
+function renderMap(latitude, longitude) {
+  if (map !== null) {
+    map.remove();
+  }
+  map = L.map("map", {
+    zoomControl: false,
+  }).setView([latitude, longitude], 15);
+
+  L.tileLayer(
+    "https://tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=RggH9KMxkJVScMskDf8JGjxgC54E662d2UBref66FozInXfB46D4W2z9iVV3AJyL",
+    {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }
+  ).addTo(map);
+
+  var myIcon = L.icon({
+    iconUrl: "./images/marker.svg",
+    iconSize: [46, 56],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  });
+
+  L.marker([latitude, longitude], { icon: myIcon }).addTo(map);
+}
+
+function updateUI(ip, city, country, postal, timezone, org) {
+  address.textContent = ip;
+  loc.textContent = `${city}, ${country} ${postal}`;
+  tmz.textContent = timezone;
+  isp.textContent = org;
+}
+
+fetchIpData();
 searchTrackerBtn.addEventListener("click", fetchIpData);
